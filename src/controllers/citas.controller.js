c37d625ef7abd263adaddbdfa7f8af33555b7ed3
cassidy <mariano.casidy.11m@gmail.com>
2023-05-28 16:21:48 -0600

    Practica interfaz con JQUERY

--- src/controllers/citas.controller.js
+++ src/controllers/citas.controller.js
@@ -1,8 +1,34 @@
 const citasController = {};
 const Citas = require('../models/Cita.model');
 
-citasController.obtenerCitas =async(req,res)=>{
+citasController.obtenerCitas = async(req,res)=>{
     const citas = await Citas.find();
-    res.json(citas);
+    res.render('../src/views/index.ejs',{citas});
 };
+
+citasController.obtenerCita = async(req,res)=>{
+    const cita = await Citas.find({ficha:req.params.cb});
+    if(cita != null){
+        res.render('../src/views/editar.ejs',{cita});
+    }else
+        res.send('{"status":"Cita no encontrada"}');
+};
+
+citasController.insertarCita = async(req,res)=>{
+    const citaInsertada = new Citas(req.body);
+    await citaInsertada.save();
+    res.redirect("/"); 
+}
+
+citasController.actualizarCita = async(req,res)=>{
+    const citaActualizada = new Citas(req.body);
+    await Citas.findOneAndUpdate({ficha:req.params.cb},{citaActualizada});
+    res.json('{"status":"Cita actualizada"}');
+}
+
+citasController.eliminarCita = async(req,res)=>{
+    await Citas.findOneAndDelete({ficha:req.params.cb});
+    res.json('{"status":"Cita eliminada"}');
+}
+
 module.exports = citasController;
--- src/routes/citas.routes.js
+++ src/routes/citas.routes.js
@@ -2,4 +2,8 @@ const {Router} = require('express');
 const citasController = require('../controllers/citas.controller');
 const router = Router();
 router.get('/',citasController.obtenerCitas);
+router.get('/:cb',citasController.obtenerCita);
+router.post('/insertar',citasController.insertarCita);
+router.put('/actualizar/:cb',citasController.actualizarCita);
+router.delete('/eliminar/:cb',citasController.eliminarCita);
 module.exports = router;
